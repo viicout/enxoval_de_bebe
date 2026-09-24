8f17dcd704424b2799ec84c0d525c59dd73bf982
Vinicius <viicout@gmail.com>
2026-09-23 23:10:17 -0300

    ...

---
+++ script.js
@@ -0,0 +1,44 @@
+const filtros = document.querySelectorAll(".filter");
+const cards = document.querySelectorAll(".gift-card");
+
+filtros.forEach(filtro => {
+
+    filtro.addEventListener("click", () => {
+
+        // Remove o active de todos os botões
+        filtros.forEach(botao => {
+            botao.classList.remove("active");
+        });
+
+        // Ativa o botão clicado
+        filtro.classList.add("active");
+
+        // Pega o texto do botão
+        const categoriaSelecionada = filtro.textContent.trim();
+
+        // Mostra ou esconde os cards
+        cards.forEach(card => {
+
+            const categoria = card
+                .querySelector(".category")
+                .textContent
+                .trim();
+
+            if (
+                categoriaSelecionada === "Todos" ||
+                categoria === categoriaSelecionada
+            ) {
+
+                card.style.display = "block";
+
+            } else {
+
+                card.style.display = "none";
+
+            }
+
+        });
+
+    });
+
+});
